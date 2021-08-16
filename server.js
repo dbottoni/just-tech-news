cda56e481bfb5256fca8318bfa5584d2b1138d04
Dana Bottoni <danabottoni@gmail.com>
2021-08-16 18:45:29 -0400

    seed files

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
@@ -14,15 +13,11 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers'));
+app.use(require('./controllers/'));
 
-// turn on routes
-//app.use(routes);
-
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
