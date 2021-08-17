fa873e1de825f21a69925f65592cca6faf67ff7c
Dana Bottoni <danabottoni@gmail.com>
2021-08-17 16:00:30 -0400

    heroku

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-
+    
       res.render('homepage', { posts });
     })
     .catch(err => {
